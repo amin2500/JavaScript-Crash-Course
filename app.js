fe9813b189f591d3e8b5afa3363d63adaedee1c2
amin <aminibrahim25@outlook.com>
2024-02-20 23:20:27 -0500

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-/**
- * Write a for-loop that loops through 1 to 15.
- * If the number is odd, print "Your Last Name"
- * if the  number is even print "Your Frist Name"
- * if the number is divisible by 5, print "Frist/Last Name"
- */
-
-for (let i = 1; i <= 15; ++i){
-    if(i % 5 === 0){
-        console.log(`${i} Amin Ibrahim`)
-    }
-    
-    else if(i % 2 === 0){
-        console.log(`${i} Amin`)
-    }
-
-    else{
-        console.log(`${i} Ibrahim`)
-    }
+function greetuser (time,name){
+ console.log(`Good ${time}, ${name}`)
 }
-// 
+
+
+greetuser ("Morning", "Amin"  )
